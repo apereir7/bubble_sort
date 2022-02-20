c5a3169be439bca545daa3d98fbc2c57d3cb40d6
Anthony Pereira <pereiratony@rocektmail.com>
2022-02-20 11:36:56 -0600

    start bubble sort

---
+++ bubble_sort.rb
@@ -0,0 +1,24 @@
+require "pry-byebug"
+
+
+def bubble_sort (array)
+    
+
+
+end
+
+
+set = [0,2,2,3,4,78]
+
+
+def compare_num (array)
+    not_sorted = false
+    array.each_with_index.map do |num, index|
+        if num > array[index +1]
+
+        end
+    end
+end
+
+
+compare_num([3,2,1])

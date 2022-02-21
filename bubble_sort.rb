0861e8d90ad857b681138640a109f3cbd13a5f52
Anthony Pereira <pereiratony@rocektmail.com>
2022-02-20 21:32:34 -0600

    further work

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,24 +1,49 @@
 require "pry-byebug"
 
 
+
+
+
+set = [4,3,78,2,0,2]
+
+
+
+
+
+
+
 def bubble_sort (array)
-    
-
-
+    not_sorted = false
+    k_iterations = (array.length() - 1)
+    iterate(array,k_iterations, not_sorted)
 end
 
 
-set = [0,2,2,3,4,78]
 
 
-def compare_num (array)
-    not_sorted = false
+
+
+
+def iterate (array,iterator, sorted)
     array.each_with_index.map do |num, index|
-        if num > array[index +1]
-
+        if (sorted && iterator == 0 )
+            p array
+            exit
+        elsif ( index == iterator  )
+            iterator -= 1
+            iterate(array,iterator,sorted)
+        elsif num > array[index +1]
+            sorted = true
+            array[index] = array[index + 1]
+            array[index + 1] = num
+        elsif num <=  array[index +1]
+            next
         end
     end
+    array
 end
 
 
-compare_num([3,2,1])
+byebug
+bubble_sort([1,2,3])
+
